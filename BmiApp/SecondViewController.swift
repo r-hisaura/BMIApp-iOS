a7e465c0061f8020a3358ef9e8340ee484bc18bc
r-hisaura <r-hisaura@cries.co.jp>
2019-02-19 13:24:40 +0900

    Initial Commit

---
+++ BmiApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BmiApp
+//
+//  Created by Cries Inc. Sankei on 2019/02/19.
+//  Copyright © 2019年 r-hisaura. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

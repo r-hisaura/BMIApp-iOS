2d9140932bd9d98b3bfdf26fb8ac811f9da10778
r-hisaura <r-hisaura@cries.co.jp>
2019-03-12 14:45:16 +0900

    １画面目レイアウト作成

--- BmiApp/InputViewController.swift
+++ BmiApp/InputViewController.swift
@@ -1,14 +1,12 @@
-//
-//  FirstViewController.swift
-//  BmiApp
-//
+
+//  入力画面コントローラ
 //  Created by Cries Inc. Sankei on 2019/02/19.
 //  Copyright © 2019年 r-hisaura. All rights reserved.
 //
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class InputViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
